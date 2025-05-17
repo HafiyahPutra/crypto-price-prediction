0fae99862eecf91df75d71a6630538999b15d2b8
Hafiyah <haffiyanputra@gmail.com>
2025-05-18 01:34:40 +0700

    30 days

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@
 API_KEY = "CG-7pi9DCcf6E6PmCFBLrwvGtZT"
 BASE_URL = "https://api.coingecko.com/api/v3/coins"
 VS_CURRENCY = "usd"
-DAYS = 60      # Jumlah hari data historis yang diambil
+DAYS = 30      # Jumlah hari data historis yang diambil
 
 WINDOW_SIZE = 7       # Jumlah hari data input untuk prediksi
 PRED_DAYS = 7         # Jumlah hari yang diprediksi ke depan
